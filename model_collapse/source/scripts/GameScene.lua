3b5a76ada595798b5458996bb650fb6d2ab74a45
TheCoggie <cogwrite@gmail.com>
2025-04-28 10:24:55 +0100

    Add files via upload
    it works now

--- model_collapse/source/scripts/GameScene.lua
+++ model_collapse/source/scripts/GameScene.lua
@@ -7,20 +7,20 @@ ldtk.load("levels/labs.ldtk", false)
 class('GameScene').extends()
 
 function GameScene:init()
-    self:goToLevel("level_0")
+    self:goToLevel("Level_0")
 
 end
 
 -- Moving levels --
-function GameScene:goToLevel(level_0)
+function GameScene:goToLevel(level_name)
     -- Removing sprites stops lag and clutter.
     -- As everything is made of sprites, below is a fast way to get rid of them when you move levels.
     gfx.sprite.removeAll()
 
     -- layer loop --
-    for layer_name, layer in pairs(ldtk.get_layers(level_0)) do
-        if layer.tiles then 
-            local tilemap = ldtk.create_tilemap(level_0, walls)
+    for layer_name, layer in pairs(ldtk.get_layers(level_name)) do
+        if layer.tiles then
+            local tilemap = ldtk.create_tilemap(level_name, layer_name)
 
             -- this is the Playdate tile map object that causes the sprite to draw the tile map --
             local layerSprite = gfx.sprite.new()
@@ -30,9 +30,9 @@ function GameScene:goToLevel(level_0)
             layerSprite:setZIndex(layer.zIndex)
             layerSprite:add()
 
-            local emptyTiles = ldtk.get_empty_tileIDs(level_0, "Solid", walls)
+            local emptyTiles = ldtk.get_empty_tileIDs(level_name, "Solid", layer_name)
             if emptyTiles then
-                gfx.sprite.addWallSprites(tilemap, emptyTiles)
+                gfx.sprite.addWallSprites(tilemap, emptyTiles) -- "source/levels/tileset-table-16-16.png"
             end
         end
     end
